7aa8cc761b08476cf1e857a9d82173c67a861fa6
Fábio Mendes <fabiobends@gmail.com>
2023-04-12 14:50:32 -0300

    refactor(Spell): make `Spell` class receive named attributes
    - Make class receive named attribues instead of ordered ones.
    - Pass `zombies` when creating a spell instead of assigning it after
    creation.
    - Remove unused constructor methods in all spell-derived classes.
    - Fix some linting errors.

--- src/spell.js
+++ src/spell.js
@@ -3,15 +3,17 @@ class Spell {
   // icon - key
   // cooldown - amount in seconds before the spell can be cast again
   // duration - amount in seconds for spells with duration eg, enchantments
-  constructor(game, x, y, icon, cooldown, duration) {
+  constructor({ game, x, y, icon, cooldown, duration, zombies }) {
     // we are not using the Phaser's group class
     // because we want the z-order to work with some sprites
     // so many of the spell sprites are added to the game.world
     // but it's still handy to put them all in an array so we can just iterate each
     this.game = game;
-    this.group = []
+    this.group = [];
     // boolean flag that enables/disable casting of the spell again
     this.active = true;
+    // zombies that might target by the spell
+    this.zombies = zombies;
     // cooldown timer
     this.cooldown = cooldown;
     // some spells, like firewall have a duration
@@ -22,8 +24,6 @@ class Spell {
 
     // and then create the necessary sprites, animations and emitters
     this.create();
-
-    console.log(this)
   }
 
   // to be overridden by child classes
@@ -44,44 +44,44 @@ class Spell {
 
     // activate the cooldown animation
     this.icon.pie.progress = 0;
-    let tween = this.game.add.tween(this.icon.pie).to({progress: 1}, this.cooldown, Phaser.Easing.Quadratic.InOut, true, 0);
+    const tween = this.game.add.tween(this.icon.pie).to({progress: 1}, this.cooldown, Phaser.Easing.Quadratic.InOut, true, 0);
     tween.onComplete.add(() => {
       this.active = true;
     });
 
     // expire the spell if it has a duration
-    if(this.duration) {
+    if (this.duration) {
       this.game.time.events.add(this.duration, () => {
         this.expire();
       });
     }
+    return true;
   }
 
   createIcon(x, y, key) {
-    console.log(this)
     // create a group for the icon so they don't get sorted along with the sprites in the this.game.world
-    let iconGroup = this.game.add.group();
+    const iconGroup = this.game.add.group();
 
     // position the icon
-    let icon = this.game.add.sprite(x, y, key);
+    const icon = this.game.add.sprite(x, y, key);
     icon.anchor.setTo(0.5, 0.5);
 
     // create a progress pie on top of the icon
-    let pie = new PieProgress(this.game, icon.x, icon.y, 40, '0x000000');
+    const pie = new PieProgress(this.game, icon.x, icon.y, 40, '0x000000');
     pie.alpha = 0.5;
 
     // put a circle frame so we have rounded spell icons
-    let g = this.game.add.graphics(0, 0);
-    let radius = 40;
+    const g = this.game.add.graphics(0, 0);
+    const radius = 40;
     g.lineStyle(20, 0x000000, 1);
     g.anchor.setTo(0, 0);
-    let xo = icon.x;
-    let yo = icon.y;
-    g.moveTo(xo,yo + radius);
-    for (let i = 0; i <= 360; i++){
-      let x = xo+ Math.sin(i * (Math.PI / 180)) * radius;
-      let y = yo+ Math.cos(i * (Math.PI / 180)) * radius;
-      g.lineTo(x,y);
+    const xo = icon.x;
+    const yo = icon.y;
+    g.moveTo(xo, yo + radius);
+    for (let i = 0; i <= 360; i++) {
+      const x = xo + Math.sin(i * (Math.PI / 180)) * radius;
+      const y = yo + Math.cos(i * (Math.PI / 180)) * radius;
+      g.lineTo(x, y);
     }
     iconGroup.add(icon);
     iconGroup.add(g);
