2724fb1df905d0592abfae9f654edcc1e15e6a65
Fábio Mendes <fabiobends@gmail.com>
2023-04-12 10:23:36 -0300

    fix: remove unused declarations

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import 'babel-polyfill';
 import DemoState from 'demo';
+
 class Game extends Phaser.Game {
   constructor() {
     var body = document.body,
@@ -17,16 +18,3 @@ class Game extends Phaser.Game {
 const game = new Game();
 game.state.add('DemoState', DemoState, false);
 game.state.start('DemoState');
-
-var myModal = new bootstrap.Modal(document.getElementById('modalCoverExample'), {
-  keyboard: false
-})
-
-$('#new_scene').on('click', function () {
-  window.location.reload();
-})
-
-
-document.body.oncontextmenu = function preventRightClick(e) {
-  e.preventDefault();
-};
