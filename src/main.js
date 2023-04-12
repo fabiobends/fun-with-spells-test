a5b8e840298130c1ad559c43d4451a369e7fbcee
Fábio Mendes <fabiobends@gmail.com>
2023-04-12 15:21:37 -0300

    fix: remove unused packages and use `const` for constant variables

--- src/main.js
+++ src/main.js
@@ -3,17 +3,26 @@ import DemoState from 'demo';
 
 class Game extends Phaser.Game {
   constructor() {
-    var body = document.body,
-      html = document.documentElement;
-    var height = Math.max(body.scrollHeight, body.offsetHeight,
-      html.clientHeight, html.scrollHeight, html.offsetHeight);
-    var width = Math.max(body.scrollWidth, body.offsetWidth,
-      html.clientWidth, html.scrollWidth, html.offsetWidth);
+    const body = document.body;
+    const html = document.documentElement;
+    const height = Math.max(
+      body.scrollHeight,
+      body.offsetHeight,
+      html.clientHeight,
+      html.scrollHeight,
+      html.offsetHeight,
+    );
+    const width = Math.max(
+      body.scrollWidth,
+      body.offsetWidth,
+      html.clientWidth,
+      html.scrollWidth,
+      html.offsetWidth,
+    );
 
     super(width, height, Phaser.AUTO, 'test', null, true, false);
   }
 }
-
 
 const game = new Game();
 game.state.add('DemoState', DemoState, false);
